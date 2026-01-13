9f0f6448bd46d520e9fd880d04606ea5d31cf62e
Jialin Huang <jialin.huang@shopee.com>
2026-01-13 12:47:39 +0800

    feat: support login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Bmob from "hydrogen-js-sdk"
 import Homepage from './pages/Homepage'
 import Chat from './pages/Chat'
 import ProfilePage from './pages/settings/profile'
+import UserAuthPage from './pages/settings/user'
 import ReportLoading from './pages/report/ReportLoading'
 import ReportResult from './pages/report/Result'
 import { ToastProvider } from './components/Toast'
@@ -9,6 +11,10 @@ import { ReportProvider } from './contexts/ReportContext'
 import './index.css'
 
 function App() {
+  const appID = import.meta.env.VITE_BMOB_APP_ID;
+  const apiSecCode = import.meta.env.VITE_BMOB_API_SEC_CODE;
+  Bmob.initialize(appID, apiSecCode);
+
   return (
     <ReportProvider>
       <ToastProvider>
@@ -17,6 +23,7 @@ function App() {
             <Route path="/" element={<Homepage />} />
             <Route path="/chat" element={<Chat />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<UserAuthPage />} />
             <Route path="/report-loading" element={<ReportLoading />} />
             <Route path="/report-result" element={<ReportResult />} />
           </Routes>
