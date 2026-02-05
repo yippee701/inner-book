02efcde65ea2e29c31c60c90e218c8221091935f
Jialin Huang <jialin.huang@shopee.com>
2026-02-05 19:01:24 +0800

    update: add global user behavior report - visit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Chat from './pages/chat/Chat'
@@ -11,28 +12,39 @@ import ShareLanding from './pages/share/shareLanding'
 import { ToastProvider } from './components/Toast'
 import { ReportProvider } from './contexts/ReportContext'
 import { CloudbaseProvider } from './contexts/cloudbaseContext'
+import { trackVisitEvent } from './utils/track'
 import './index.css'
+
+/** 进入应用时上报一次全局访问 */
+function VisitAppTracker({ children }) {
+  useEffect(() => {
+    trackVisitEvent('visit_app')
+  }, [])
+  return children
+}
 
 function App() {
   return (
     <CloudbaseProvider>
-      <ToastProvider>
-        <ReportProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/chat-history" element={<ChatHistory />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/report-loading" element={<ReportLoading />} />
-              <Route path="/report-result" element={<ReportResult />} />
-              <Route path="/share" element={<ShareLanding />} />
-            </Routes>
-          </Router>
-        </ReportProvider>
-      </ToastProvider>
+      <VisitAppTracker>
+        <ToastProvider>
+          <ReportProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/chat-history" element={<ChatHistory />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/report-loading" element={<ReportLoading />} />
+                <Route path="/report-result" element={<ReportResult />} />
+                <Route path="/share" element={<ShareLanding />} />
+              </Routes>
+            </Router>
+          </ReportProvider>
+        </ToastProvider>
+      </VisitAppTracker>
     </CloudbaseProvider>
   )
 }
